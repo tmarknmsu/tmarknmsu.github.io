e11c26359f0151b4817ffa248d24726d17cf7b7a
tmarknmsu <tmark07@gmail.com>
2022-10-03 17:14:58 -0600

    Update 10/03/2022 5:15PM

--- ICT4300_week4_Discussion_folder/ICT4300_week4_Discussion_js.js
+++ ICT4300_week4_Discussion_folder/ICT4300_week4_Discussion_js.js
@@ -1,16 +1,33 @@
 'use strict';
-const switcher = document.querySelector('.btn');
+const switcher = document.querySelector('.ndBtn');
 switcher.addEventListener('click',function(){
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
+    document.body.classList.toggle('irish-theme');
     const className = document.body.className;
-    if(className == "light-theme")
-        {this.textContent = "Dark";}
-    else 
-        {this.TEXTcONTENT = "Light";}
-
-    console.log('current class name: '+className);
+    console.log('current class name: '+ className);
 });
+    // const switcher = document.querySelector('#iowaBtn');
+    // switcher.addEventListener('click',function(){
+    //     document.body.classList.toggle('hawkeyes-theme');
+    //     const className = document.body.className;
+    //     console.log('current class name: '+ className);
+    // });
 
+    // const switcher = document.querySelector('#nmsuBtn');
+    // switcher.addEventListener('click',function(){
+    //     document.body.classList.toggle('aggies-theme');
+    //     const className = document.body.className;
+    //     if(className == "aggies-theme")
+
+    //     console.log('current class name: '+ className);
+    // });
+
+    // const switcher = document.querySelector('#cuBtn');
+    // switcher.addEventListener('click',function(){
+    //     document.body.classList.toggle('buffaloes-theme');
+
+    //     const className = document.body.className;
+    //     if(className == "buffaloes-theme")
+
+    //     console.log('current class name: '+ className);
+    // });
 
