4a125a59f74ac233a0cad9b44b2cf51e89efb816
tmarknmsu <tmark07@gmail.com>
2022-10-03 20:33:15 -0600

    final update 10/03/2022 08:33 PM

--- ICT4300_week4_Discussion_folder/ICT4300_week4_Discussion_js.js
+++ ICT4300_week4_Discussion_folder/ICT4300_week4_Discussion_js.js
@@ -1,33 +1,86 @@
 'use strict';
-const switcher = document.querySelector('.ndBtn');
-switcher.addEventListener('click',function(){
-    document.body.classList.toggle('irish-theme');
+
+var notreDamejs= document.getElementsByClassName("notreDame")[0];
+var iowajs= document.getElementsByClassName("iowa")[0];
+var nmsujs= document.getElementsByClassName("nmsu")[0];
+var coloradojs= document.getElementsByClassName("colorado")[0];
+
+const irish = document.querySelector('.ndBtn');
+irish.addEventListener('click',function(){
+    document.body.classList.add('irish-theme');
+        document.body.classList.remove('buffaloes-theme','hawkeyes-theme','aggies-theme');
     const className = document.body.className;
     console.log('current class name: '+ className);
+    var coloradojs= document.getElementsByClassName("colorado")[0];
+        console.log(coloradojs.style)
+            coloradojs.style.display = "none"
+    var iowajs= document.getElementsByClassName("iowa")[0];
+        console.log(iowajs.style)
+        iowajs.style.display = "none"
+    var nmsujs= document.getElementsByClassName("nmsu")[0];
+        console.log(nmsujs.style)
+        nmsujs.style.display = "none"
+    var notreDamejs= document.getElementsByClassName("notreDame")[0];
+        console.log(notreDamejs.style)
+        notreDamejs.style.display = "block"
 });
-    // const switcher = document.querySelector('#iowaBtn');
-    // switcher.addEventListener('click',function(){
-    //     document.body.classList.toggle('hawkeyes-theme');
-    //     const className = document.body.className;
-    //     console.log('current class name: '+ className);
-    // });
+    const hawkeyes = document.querySelector('.iowaBtn');
+    hawkeyes.addEventListener('click',function(){
+        document.body.classList.add('hawkeyes-theme');
+        document.body.classList.remove('buffaloes-theme','irish-theme','aggies-theme');
+        const className = document.body.className;
+        console.log('current class name: '+ className);
+    var coloradojs= document.getElementsByClassName("colorado")[0];
+        console.log(coloradojs.style)
+            coloradojs.style.display = "none"
+    var iowajs= document.getElementsByClassName("iowa")[0];
+        console.log(iowajs.style)
+        iowajs.style.display = "block"
+    var nmsujs= document.getElementsByClassName("nmsu")[0];
+        console.log(nmsujs.style)
+        nmsujs.style.display = "none"
+    var notreDamejs= document.getElementsByClassName("notreDame")[0];
+        console.log(notreDamejs.style)
+        notreDamejs.style.display = "none"
+    });
 
-    // const switcher = document.querySelector('#nmsuBtn');
-    // switcher.addEventListener('click',function(){
-    //     document.body.classList.toggle('aggies-theme');
-    //     const className = document.body.className;
-    //     if(className == "aggies-theme")
+    const aggies = document.querySelector('.nmsuBtn');
+    aggies.addEventListener('click',function(){
+        document.body.classList.add('aggies-theme');
+        document.body.classList.remove('hawkeyes-theme','irish-theme','buffaloes-theme');
+    var coloradojs= document.getElementsByClassName("colorado")[0];
+        console.log(coloradojs.style)
+            coloradojs.style.display = "none"
+    var iowajs= document.getElementsByClassName("iowa")[0];
+        console.log(iowajs.style)
+        iowajs.style.display = "none"
+    var nmsujs= document.getElementsByClassName("nmsu")[0];
+        console.log(nmsujs.style)
+        nmsujs.style.display = "block"
+    var notreDamejs= document.getElementsByClassName("notreDame")[0];
+        console.log(notreDamejs.style)
+        notreDamejs.style.display = "none"
+        
 
-    //     console.log('current class name: '+ className);
-    // });
+        
+    });
 
-    // const switcher = document.querySelector('#cuBtn');
-    // switcher.addEventListener('click',function(){
-    //     document.body.classList.toggle('buffaloes-theme');
-
-    //     const className = document.body.className;
-    //     if(className == "buffaloes-theme")
-
-    //     console.log('current class name: '+ className);
-    // });
+    const buffaloes = document.querySelector('.cuBtn');
+    buffaloes.addEventListener('click',function(){
+        document.body.classList.add('buffaloes-theme');
+        document.body.classList.remove('hawkeyes-theme','irish-theme','aggies-theme');
+        var coloradojs= document.getElementsByClassName("colorado")[0];
+            console.log(coloradojs.style)
+                coloradojs.style.display = "block"
+        var iowajs= document.getElementsByClassName("iowa")[0];
+            console.log(iowajs.style)
+            iowajs.style.display = "none"
+        var nmsujs= document.getElementsByClassName("nmsu")[0];
+            console.log(nmsujs.style)
+            nmsujs.style.display = "none"
+        var notreDamejs= document.getElementsByClassName("notreDame")[0];
+            console.log(notreDamejs.style)
+            notreDamejs.style.display = "none"
+            
+    });
 
