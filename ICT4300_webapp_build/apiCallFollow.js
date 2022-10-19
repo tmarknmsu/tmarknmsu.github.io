2c4c5f2fd96769e970df724ae7f77f99e0b7e3fc
tmarknmsu <tmark07@gmail.com>
2022-10-19 11:48:26 -0600

    not working really yet

--- ICT4300_webapp_build/apiCallFollow.js
+++ ICT4300_webapp_build/apiCallFollow.js
@@ -1,14 +1,15 @@
 function getBooks(){
 	document.getElementById('output').innerHTML="";
-	fetch("http://openlibrary.org/search.json?q="+document.getElementById("input").value)
+	fetch("http://openlibrary.org/search.json?q="+document.getElementById("myInput").value)
 	.then(a=> a.json())
 	.then(response =>{
-		for(var i=0; i<100; i+=1)
+		for(var i=0; i<10; i+=1)
 		{
 			// gets the title
 			document.getElementById("output").innerHTML+="<h1>"+response.docs[i].title+"</h1>"+
 			// gets the author
 			response.docs[i].author_name[0]+"<br>"+
+			// gets author key
 			response.docs[i].author_key[0]+
 			// gets the book cover
 			"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-S.jpg'><br>";
@@ -17,4 +18,46 @@ function getBooks(){
 
 
 	});
-}
+};
+
+
+var input = document.getElementById("myInput")
+input.addEventListener("keypress", function(event) {
+	if(event.key == "Enter"){
+		event.preventDefault();
+		document.getElementById("mySearch").click;
+		
+	}
+
+});
+
+
+
+
+
+
+// document.querySelector("#addLinks").addEventListener("keyup", event => {
+//     if(event.key !== "Enter") return; // Use `.key` instead.
+//     document.querySelector("#linkadd").click();
+// 	document.getElementById('output').innerHTML="";
+// 		fetch("http://openlibrary.org/search.json?q="+document.getElementById("input").value)
+// 		.then(a=> a.json())
+// 		.then(response =>{
+// 			for(var i=0; i<10; i+=1)
+// 			{
+// 				// gets the title
+// 				document.getElementById("output").innerHTML+="<h1>"+response.docs[i].title+"</h1>"+				
+// 				// gets the author
+// 				response.docs[i].author_name[0]+"<br>"+
+// 				// gets author key
+// 				response.docs[i].author_key[0]+
+// 				// gets the book cover
+// 				"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-S.jpg'><br>";
+
+// 			}
+
+
+// 			});
+// 	 // Things you want to do.
+//     event.preventDefault(); // No need to `return false;`.
+// });
