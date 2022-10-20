a8306122dcc238c090d032b09e33818411d854fa
Mark Thompson <tmark07@gmail.com>
2022-10-19 21:39:55 -0600

    got the enter key thing working somehow

--- ICT4300_webapp_build/ICT4300_webapp.js
+++ ICT4300_webapp_build/ICT4300_webapp.js
@@ -21,3 +21,40 @@ document.getElementById("main").style.marginLeft = "25%";
 document.getElementById("title").style.marginLeft = "0";
 }
 
+function getBooks(){
+	document.getElementById('output').innerHTML="";
+	fetch("http://openlibrary.org/search.json?q="+document.getElementById("myInput").value)
+	.then(a=> a.json())
+	.then(response =>{
+		for(var i=0; i<10; i+=1)
+		{
+			// gets the title
+			document.getElementById("output").innerHTML+="<h1>"+response.docs[i].title+"</h1>"+
+			// gets the author
+			response.docs[i].author_name[0]+"<br>"+
+			// gets author key
+			response.docs[i].author_key[0]+
+			// gets the book cover
+			"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-S.jpg'><br>";
+			
+
+
+		}
+
+
+	});
+	document.getElementById("boxTitle").style.fontSize = "0";};
+
+let input = document.getElementById("myInput");
+let searchButton = document.getElementById('mySearch');
+
+input.addEventListener("keypress", function(event) {
+	if(event.key == "Enter"){
+		event.preventDefault();
+		searchButton.click();
+		// getBooks();
+		console.log("atleast it registered");
+		
+	}
+
+});
