e2fca2afbd81467a48fba900621d858f38420582
tmarknmsu <tmark07@gmail.com>
2022-11-06 17:16:37 -0700

    played with the UUID function

--- ICT4300_webapp_build/ICT4300_webapp.py
+++ ICT4300_webapp_build/ICT4300_webapp.py
@@ -1,5 +1,29 @@
 #%%
 import uuid
+import pandas as pd
 uuidtest = uuid.uuid4()
 
 print(uuidtest)
+
+def id(i):
+    identifier = uuid.uuid4()
+    print(identifier)
+    id_list = [i,identifier]
+    print(id_list)
+    return id_list
+
+df = pd.DataFrame(data = {'UUID':[0]})
+
+iterations = 5
+i =0
+
+while i <= iterations:
+    id_counter = []
+    # print(id)
+    # df = pd.DataFrame(id_counter, columns = ['count','UUID'])
+    identifier = uuid.uuid4()
+    # id_counter.append(identifier)
+    print(identifier)
+    i= i+1
+
+
