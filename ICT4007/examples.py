29489d95f703609765d64c3c0973cb958c7d9c3f
tmarknmsu <tmark07@gmail.com>
2022-11-08 13:50:28 -0700

    ;i

---
+++ ICT4007/examples.py
@@ -0,0 +1,86 @@
+# Just a  bunch of examples for python
+#%%
+age = 20
+name = 'Swaroop'
+
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+
+print('Why is {0} playing with that python?'.format(name))
+
+
+# %%
+age = 20
+name = 'Swaroop'
+
+print('{name} was {age} years old when he wrote this book'.format(name=name, age=age))
+print('Why is {name} playing with that python?'.format(name=name))
+
+#%%
+# decimal (.) precision of 3 for float '0.333'
+print('{0:.3f}'.format(1.0/3))
+# fill with underscores (_) with the text centered
+# (^) to 11 width '___hello___'
+print('{0:_^11}'.format('hello'))
+# keyword-based 'Swaroop wrote A Byte of Python'
+print('{name} wrote {book}'.format(name='Swaroop', book='A Byte of Python'))
+
+#%% to skip line break at end of print
+
+print('a', end='')
+print('b', end='')
+
+print('a', end=' ')
+print('b', end=' ')
+print('c')
+
+#%%
+# Filename : var.py
+i = 5
+print(i)
+i = i + 1
+print(i)
+
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
+#%%
+
+number = 23
+guess = int(input('Enter an integer : '))
+
+if guess == number:
+    # New block starts here
+    print('Congratulations, you guessed it.')
+    print('(but you do not win any prizes!)')
+    # New block ends here
+elif guess < number:
+    # Another block
+    print('No, it is a little higher than that')
+    # You can do whatever you want in a block ...
+else:
+    print('No, it is a little lower than that')
+    # you must have guessed > number to reach here
+
+print('Done')
+# This last statement is always executed,
+# after the if statement is executed.
+#%%
+number = 23
+running = True
+
+while running:
+    guess = int(input('Enter an integer : '))
+
+    if guess == number:
+        print('Congratulations, you guessed it.')
+        # this causes the while loop to stop
+        running = False
+    elif guess < number:
+        print('No, it is a little higher than that.')
+    else:
+        print('No, it is a little lower than that.')
+else:
+    print('The while loop is over.')
+    # Do anything else you want to do here
+
+print('Done')
